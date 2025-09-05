bf641fa8aba69abffb345ed3da5bcfa866e2cf93
Pavle_io23@student.itstep.org <Pavle_io23@student.itstep.org>
2025-09-05 11:17:46 +0300

    1321

--- Project5/FileName.cpp
+++ Project5/FileName.cpp
@@ -1,33 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class Fraction
-{
-private:
-    int x;
-    int y;
-    char sumbol;
-public:
-    void Print()
-    {
-        cout <<  sumbol  << " fraction -> " << x << " / " << y << endl;
-    }
-    void Init(char _t, int _x, int _y)
-    {
-        sumbol = _t;
-        x = _x;
-        y = _y;
-    }
-};
 int main()
 {
-    Fraction a, b;
-
-
-    a.Init('1', 10, -2);
-    a.Print();
-
-    b.Init('2', 5, 16);
-    b.Print();
-
+	cout << "Hello world";
 }
